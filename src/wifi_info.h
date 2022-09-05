6f5315da567fb369f5d519566d9c7b9dd0021b2d
Louis <88962972+LouisLee985@users.noreply.github.com>
2022-09-05 13:26:48 +0800

    Update wifi_info.h

--- src/wifi_info.h
+++ src/wifi_info.h
@@ -10,17 +10,19 @@
 const char *ssid = "mywifissid";
 const char *password = "mypassword ";
 
-void wifi_connect() {
-    WiFi.persistent(false);
-    WiFi.mode(WIFI_STA);
-    WiFi.setAutoReconnect(true);
-    WiFi.begin(ssid, password);
-    Serial.println("WiFi connecting...");
-    while (!WiFi.isConnected()) {
-        delay(10); 
-        Serial.print(".");        
-    }
-    Serial.println();
+void wifi_connect()
+{
+	WiFi.persistent(false);
+	WiFi.mode(WIFI_STA);
+	WiFi.setAutoReconnect(true);
+	WiFi.begin(ssid, password);
+	Serial.println("WiFi connecting...");
+	while (!WiFi.isConnected())
+	{
+		delay(10);
+		Serial.print(".");
+	}
+	Serial.println();
 }
 
 #endif /* _WIFI_INFO_H_ */
