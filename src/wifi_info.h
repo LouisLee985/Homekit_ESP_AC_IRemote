acbad0f7cec52d7971e8729ff92f27e820045c2d
Louis <88962972+LouisLee985@users.noreply.github.com>
2022-09-25 18:10:03 +0800

    Update wifi_info.h

--- src/wifi_info.h
+++ src/wifi_info.h
@@ -12,17 +12,23 @@ const char *password = "mypassword ";
 
 void wifi_connect()
 {
-	WiFi.persistent(false);
-	WiFi.mode(WIFI_STA);
-	WiFi.setAutoReconnect(true);
-	WiFi.begin(ssid, password);
-	Serial.println("WiFi connecting...");
-	while (!WiFi.isConnected())
-	{
-		delay(10);
-		Serial.print(".");
-	}
-	Serial.println();
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.setAutoReconnect(true);
+  WiFi.begin(ssid, password);
+  Serial.println("WiFi connecting...");
+  while (!WiFi.isConnected())
+  {
+    delay(50);
+    Serial.print(".");
+  }
+  Serial.print("\n");
+  // Serial.printf("WiFi connected, IP: %s\n", WiFi.localIP().toString().c_str());
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID()); // WiFi
+  Serial.print("IP :\t");
+  Serial.println(WiFi.localIP()); // IP
+  Serial.print("MAC :\t");
+  Serial.println(WiFi.macAddress());
 }
-
 #endif /* _WIFI_INFO_H_ */
